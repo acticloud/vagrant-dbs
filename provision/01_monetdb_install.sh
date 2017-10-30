64ae524aa029f69e9c548bf0cfa7c7dc77efd97e
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2017-10-30 13:37:02 +0100

    Update MonetDB to the newest version
    Update MonetDB to include the Jul2017-SP2 release.  Also make the script a bit
    more parametric in order to be able to change it more easily in the future.

--- provision/01_monetdb_install.sh
+++ provision/01_monetdb_install.sh
@@ -5,25 +5,32 @@ apt-get update && apt-get upgrade -y && apt-get install -y wget
 mkdir -p /root/monetdb
 cd /root/monetdb
 
-wget https://www.monetdbsolutions.com/downloads/acticloud/acticloud_xenial_distribution_20171002.tar.bz2
-tar xvf acticloud_xenial_distribution_20171002.tar.bz2
+dlfile=acticloud_xenial_distribution_20171030
+monet_distribution=11.27.9
+
+wget https://www.monetdbsolutions.com/downloads/acticloud/${dlfile}.tar.bz2
+tar xvf ${dlfile}.tar.bz2
 
 apt-get install -y libcurl3-gnutls
 
-cd acticloud_xenial_distribution_20171002
-dpkg -i libmonetdb-stream8_11.27.5_amd64.deb
+cd ${dlfile}
+dpkg -i libmonetdb-stream8_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i libmonetdb15_11.27.5_amd64.deb
+dpkg -i libmonetdb15_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i libmonetdb-client9_11.27.5_amd64.deb
+dpkg -i libmonetdb-client9_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i monetdb5-server_11.27.5_amd64.deb
+dpkg -i monetdb5-server_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i monetdb5-sql_11.27.5_amd64.deb
+dpkg -i monetdb5-sql_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i monetdb5-sql-hugeint_11.27.5_amd64.deb
+dpkg -i monetdb5-server-hugeint_${monet_distribution}_amd64.deb
 apt-get -yf install
-dpkg -i monetdb-client_11.27.5_amd64.deb
+dpkg -i monetdb5-sql-hugeint_${monet_distribution}_amd64.deb
+apt-get -yf install
+dpkg -i monetdb-client_${monet_distribution}_amd64.deb
+apt-get -yf install
+dpkg -i monetdb-client-tools_${monet_distribution}_amd64.deb
 apt-get -yf install
 
 cat << EOF > /etc/default/monetdb5-sql
@@ -38,10 +45,13 @@ STARTUP="yes"
 DBFARM=/var/lib/monetdb
 EOF
 
-systemctl enable monetdb5-sql
-systemctl start monetdb5-sql
+systemctl enable monetdbd
+systemctl start monetdbd
+
+usermod -aG monetdb ubuntu
 
 cat << EOF > ~/.monetdb
 user=monetdb
 password=monetdb
 save_history=true
+EOF
