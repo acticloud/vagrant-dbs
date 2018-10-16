508930f2e4a301858abfe49e2296e9458a2c844c
Foivos <foivos.zakkak@manchester.ac.uk>
2018-10-16 19:35:48 +0300

    Update maxine vm installation script

--- scripts/03_maxinevm_install.sh
+++ scripts/03_maxinevm_install.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /opt/maxineVM
 cd /opt/maxineVM
-git clone https://github.com/beehive-lab/Maxine-Graal.git graal
+git clone https://github.com/graalvm/mx.git mx
+export PATH=$(pwd)/mx:$PATH
 git clone https://github.com/beehive-lab/Maxine-VM.git maxine
 cd maxine
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export MAXINE_HOME=$(pwd)
 export DEFAULT_VM=maxine
-export PATH=../graal/mxtool:$PATH
 mx build
 mx image
 chown -R ubuntu:ubuntu /opt/maxineVM
